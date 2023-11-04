2daa788adae2df970f09cab82286729645a8282d
ROUND0dev <util.round0@gmail.com>
2023-11-04 13:34:21 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,7 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
+    puts(post_params)
     @post = Post.new(post_params)
 
     respond_to do |format|
